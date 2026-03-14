7eb4b420d07f04ad00ef990170659d07edacd7c0
GrahLnn <grahlnn@duck.com>
2026-03-15 00:52:25 +0800

    refine public api ergonomics and remove facade layer

--- core/src/prelude.rs
+++ core/src/prelude.rs
@@ -1,9 +1,16 @@
-pub use crate::facade::data::{
-    get_db, init_db, init_db_with_options, query_bound, query_bound_checked, query_bound_return,
-    query_bound_take, query_checked, query_raw, query_return, query_take, relation_name, run_tx,
-    Crud, DbRuntime, GraphCrud, GraphRepo, HasId, InitDbOptions, ModelMeta, QueryKind, RawSql,
-    RawSqlStmt, RecordId, Relation, RelationMeta, Repo, Table, TxResults, TxRunner, TxStmt,
+pub use crate::auth::ensure_root_user;
+pub use crate::connection::{get_db, init_db, init_db_with_options, InitDbOptions};
+pub use crate::crypto::{CryptoContext, CryptoError};
+pub use crate::error::DBError;
+pub use crate::graph::{GraphCrud, GraphRepo, Relation};
+pub use crate::model::meta::HasId;
+pub use crate::model::relation::relation_name;
+pub use crate::query::sql::{
+    query_bound, query_bound_checked, query_bound_return, query_bound_take, query_checked,
+    query_raw, query_return, query_take, RawSqlStmt,
 };
-pub use crate::facade::security::{ensure_root_user, CryptoContext, CryptoError};
-pub use crate::facade::support::{DBError, Id};
+pub use crate::repository::Crud;
+pub use crate::serde_utils::id::Id;
+pub use crate::tx::{run_tx, TxResults, TxStmt};
+pub use surrealdb::types::{RecordId, Table};
 pub use crate::{declare_relation, impl_crud, impl_id};
